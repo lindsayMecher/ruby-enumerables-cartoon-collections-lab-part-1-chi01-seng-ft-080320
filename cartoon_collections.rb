8833539dc1b8c6eaa43ac00e325c845ca2b6d176
lindsayMecher <lindsaymecher@gmail.com>
2021-01-12 12:54:09 -0600

    commit tree 4f9a0f74056afb4792e00a4337fe2163108f0570

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,48 @@
+require "pry"
+
 def greet_characters(array)
-  array.each do |char|
-    puts "Hello #{char}!"
-  end 
+  # Use `each` to enumerate over the provided array
+  #
+  # Print a custom greeting for each element
 end
 
 def list_dwarves(array)
-  array.each_with_index do |char, idx|
-    puts "#{idx + 1}. #{char}"
-  end 
+  # Use `each_with_index` to enumerate over the provided array
+  #
+  # Print a numbered list of each element
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def greet_characters(array)
+#   array.each do |char|
+#     puts "Hello #{char}!"
+#   end 
+# end
+
+# def list_dwarves(array)
+#   array.each_with_index do |char, idx|
+#     puts "#{idx + 1}. #{char}"
+#   end 
+# end
